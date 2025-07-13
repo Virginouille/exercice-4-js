6b4ff2fc2d48c08522b38b3e56de2980c556fd9f
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-07-13 20:06:25 +0200

    micro hs

--- exercice-2/exercice2.js
+++ exercice-2/exercice2.js
@@ -9,5 +9,44 @@ requestAnimationFrame */
 
 //Mise en place AudioContest
 
-const audioContest = new AudioContext();
-const analiserNode = audioContest.createAnalyser();
+const canvas = document.getElementById("bloc_canva");
+const ctx = canvas.getContext("2d");
+
+navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stream => {
+        const audioContext = new AudioContext();
+        const source = audioContext.createMediaStreamSource(stream);
+
+        const analyserNode = audioContext.createAnalyser();
+        analyserNode.fftSize = 256;
+
+        const bufferLength = analyserNode.frequencyBinCount; // 128
+        const dataArray = new Uint8Array(bufferLength);
+
+        source.connect(analyserNode);
+
+        function draw() {
+            requestAnimationFrame(draw);
+
+            analyserNode.getByteFrequencyData(dataArray);
+
+            ctx.fillStyle = '#111';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            const barWidth = canvas.width / bufferLength;
+            let x = 0;
+
+            for (let i = 0; i < bufferLength; i++) {
+                const barHeight = dataArray[i];
+                ctx.fillStyle = `rgb(${barHeight + 100}, 50, 200)`;
+                ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+                x += barWidth + 1;
+            }
+        }
+
+        draw();
+    })
+    .catch(err => {
+        alert("Erreur : impossible d'accéder au micro");
+        console.error(err);
+    });
