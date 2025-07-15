0f88ff84fb0884f2a1b4512ee79d9b0e2467cfa0
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-07-15 13:33:51 +0200

    avancement index db

--- exercice-8/exercice-8.js
+++ exercice-8/exercice-8.js
@@ -6,7 +6,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const ajoutContact = document.getElementById("btn_ajouter");
 
     //ouvrir base
-    const base = window.indexedDB.open("nouvellebdd", 1);
+    const base = window.indexedDB.open("nouvellebdd", 2);
     console.log("nouvelle bdd : ", base);
 
     //creation d'un store
@@ -16,18 +16,17 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     //ajouter un contact à la bdd
-
     function ajouterContact() {
-
 
         base.onsuccess = function (event) {
             const bdd = event.target.result;
             console.log("bdd ouverte avec succès", bdd);
 
+            afficherContacts(bdd);
+
             ajoutContact.addEventListener("click", (event) => {
                 event.preventDefault();
                 console.log("btn cliqué", ajoutContact);
-
 
                 const transaction = bdd.transaction(["contacts"], "readwrite");
                 const stockage = transaction.objectStore("contacts");
@@ -42,6 +41,34 @@ window.addEventListener("DOMContentLoaded", () => {
         };
 
     };
+
+    /*afficher contacts*/
+    function afficherContacts(bdd) {
+
+        base.onsuccess = function (event) {
+            const bdd = event.target.result;
+            const transaction = bdd.transaction(["contacts"], "readonly");
+            const stockage = transaction.objectStore("contacts");
+
+            const requete = stockage.openCursor();
+            const listeContacts = document.getElementById("liste_contacts");
+
+            requete.onsuccess = function (event) {
+                const cursor = event.target.result;
+
+                if (cursor) {
+                    const contact = cursor.value;
+                    const li = document.createElement("li");
+                    li.textContent = `${contact.prenom} - ${contact.numero}`;
+                    listeContacts.appendChild(li);
+                    cursor.continue();
+                } else {
+                    console.log("Liste complète");
+                }
+            };
+        }
+
+    }
 
     ajouterContact();
 
